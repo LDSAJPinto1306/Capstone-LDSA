fc2a948767d73c26c1543ba00d21413e7acb3093
LDSAJPinto1306 <joao.pinto@neofin.co.uk>
2024-06-26 18:46:46 +0100

    Finally Fixing my App

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ DB = connect(os.environ.get('DATABASE_URL') or 'sqlite:///predictions.db')
 
 # Define Prediction model
 class Prediction(Model):
-    id = CharField(primary_key=True, max_length=50)
+    id = TextField(unique=True)
     observation = TextField()
     pred_class = BooleanField()
     true_class = BooleanField(null=True)
@@ -71,9 +71,9 @@ def process_observation(observation):
 def predict():
     obs_dict = request.get_json()
     logger.info('Received observation: %s', obs_dict)
-    _id = str(obs_dict.get('id'))
+    obs_id = obs_dict.get('id')
 
-    if not _id:
+    if not obs_id:
         logger.warning('No id provided in request')
         return jsonify({'error': 'id is required'}), 400
 
@@ -81,8 +81,8 @@ def predict():
     observation = process_observation(obs_dict)
 
     # Check if the ID already exists in the database
-    if Prediction.select().where(Prediction.id == _id).exists():
-        logger.warning('ID %s already exists in the database', _id)
+    if Prediction.select().where(Prediction.id == obs_id).exists():
+        logger.warning('ID %s already exists in the database', obs_id)
         return jsonify({'error': 'id already exists'}), 400
 
     try:
@@ -93,13 +93,18 @@ def predict():
 
     # Make prediction using the model
     outcome = bool(pipeline.predict(obs_df))
-    response = {'id': _id, 'outcome': outcome}
+    response = {'id': obs_id, 'outcome': outcome}
     
     # Save prediction to database
-    p = Prediction(id=_id,pred_class=outcome,observation=json.dumps(obs_dict))
-    logger.info('Stored Prediction %s',p)
-    p.save()
-
+    p = Prediction(id=obs_id,pred_class=outcome,observation=json.dumps(obs_dict))
+    try:
+        p.save()
+        logger.info('Stored Prediction %s',p)
+    except:
+        error_msg = '\n\n\n !!! Failed to Store Prediction !!! \n\n\n'
+        logger.warning(error_msg)
+        return error_msg
+    
     return jsonify(response)
 
 # Endpoint for updating true class
@@ -107,25 +112,25 @@ def predict():
 def update():
     obs = request.get_json()
     logger.info('Received update request: %s', obs)
-    _id = obs.get('id')
+    obs_id = obs.get('id')
     outcome = obs.get('outcome')
 
-    if not _id:
+    if not obs_id:
         logger.warning('No id provided in request')
         return jsonify({'error': 'id is required'}), 400
 
     # Check if the ID exists in the database
-    if not Prediction.select().where(Prediction.id == _id).exists():
-        logger.warning('ID %s does not exist in the database', _id)
+    if not Prediction.select().where(Prediction.id == obs_id).exists():
+        logger.warning('ID %s does not exist in the database', obs_id)
         return jsonify({'error': 'id does not exist'}), 400
 
     # Update true class
-    p = Prediction.get(Prediction.id == _id)
+    p = Prediction.get(Prediction.id == obs_id)
     p.true_class = outcome
     p.save()
-    logger.info('Updated true class for ID %s', _id)
+    logger.info('Updated true class for ID %s', obs_id)
 
-    response = {'id': _id, 'outcome': outcome, 'predicted_outcome': p.pred_class}
+    response = {'id': obs_id, 'outcome': outcome, 'predicted_outcome': p.pred_class}
     return jsonify(response)
 
 # Endpoint to list database contents
